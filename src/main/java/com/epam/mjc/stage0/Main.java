4cf7ce0a129936badab428e666c0cfff285430b0
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-06-05 12:05:55 +0500

    Bismillah 🥇

---
+++ src/main/java/com/epam/mjc/stage0/Main.java
@@ -0,0 +1,11 @@
+package com.epam.mjc.stage0;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        System.out.println(dog.getDescription());
+
+        Bird bird = new Bird();
+        System.out.println(bird.getDescription());
+    }
+}
